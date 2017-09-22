c5e8ca772c54df7ea711b45953bbf5a8393e0a2d
wangning0 <ran_ning@126.com>
2017-09-22 17:14:23 +0800

    update json

---
+++ ex_8/main.go
@@ -0,0 +1,46 @@
+/*
+	这个example是关于session的
+	使用了一个出名的库  gorilla/sessions
+*/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/sessions"
+)
+
+var (
+	key = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	fmt.Fprintln(w, "The cake is a lie!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+}
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+}
+func main() {
+	http.HandleFunc("/secret", secret)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+
+	http.ListenAndServe(":8080", nil)
+}
