84d3826dc9ad87192f33fdef43753e7fbe093156
wangning0 <ran_ning@126.com>
2017-09-26 21:14:49 +0800

    add rpc

---
+++ ex_28_rpc/ex_4_tcp_server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"os"
+	"net/rpc"
+	"errors"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+func main() {
+	arith := new(Arith)
+	rpc.Register(arith)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", ":1234")
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	rpc.ServeConn(conn)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
